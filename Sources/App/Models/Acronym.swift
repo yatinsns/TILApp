1f2ed1928ed38b624c7ef670d914047b469ba485
Yatin Sarbalia <yatinsns@gmail.com>
2018-08-25 23:53:20 +0530

    Use Fluent

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,28 @@
+import Vapor
+import FluentSQLite
+
+final class Acronym: Codable {
+  var id: Int?
+  var short: String
+  var long: String
+
+  init(short: String, long: String) {
+    self.short = short
+    self.long = long
+  }
+}
+
+//extension Acronym: Model {
+//  typealias Database = SQLiteDatabase
+//  typealias ID = Int
+//
+//  public static var idkey: IDKey = \Acronym.id
+//}
+
+extension Acronym: SQLiteModel {}
+
+extension Acronym: Migration {}
+
+extension Acronym: Content {}
+
+
